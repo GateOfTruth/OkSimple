d9a8efee5690ac017485cab0d099261e911cd3a4
Raven <1572777804@qq.com>
2020-12-17 00:21:31 +0800

    Update ChangeHostStrategy.kt
    修改demo

--- app/src/main/java/com/gateoftruth/sample/ChangeHostStrategy.kt
+++ app/src/main/java/com/gateoftruth/sample/ChangeHostStrategy.kt
@@ -6,9 +6,9 @@ import okhttp3.Request
 class ChangeHostStrategy : RequestUntilSuccess() {
 
     /**
-     * count>1表示请求失败，失败之后动态切换域名或者重写header都是可以的
+     * count>0表示请求失败，失败之后动态切换域名或者重写header都是可以的
      */
     override fun getRequestBuilder(builder: Request.Builder): Request.Builder {
-        return if (count > 1) builder.url("https://google.com") else builder
+        return if (count > 0) builder.url("https://google.com") else builder
     }
 }
