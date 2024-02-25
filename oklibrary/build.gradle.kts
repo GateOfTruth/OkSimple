0d6e529a514cbfca913db7cf9d8ce81abb98c157
Alen <1572777804@qq.com>
2024-02-25 22:29:13 +0800

    修改warning

--- oklibrary/build.gradle.kts
+++ oklibrary/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         minSdk = 21
-        targetSdk = 34
+        lint.targetSdk = 34
 
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
