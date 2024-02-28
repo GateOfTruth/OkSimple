6d1d02652c48e62b4a0d2ba42fe75e5ef4539ac4
Alen <1572777804@qq.com>
2024-02-28 20:17:36 +0800

    增加注释

--- app/src/main/java/com/gateoftruth/sample/GsonCallBack.kt
+++ app/src/main/java/com/gateoftruth/sample/GsonCallBack.kt
@@ -4,6 +4,9 @@ import com.gateoftruth.oklibrary.DataCallBack
 import com.google.gson.Gson
 import java.lang.reflect.ParameterizedType
 
+/**
+ * 仅供参考，实际使用中需要优化，比如可以考虑把gson设置为单例等等
+ */
 abstract class GsonCallBack<E> : DataCallBack<E>() {
     private val gson = Gson()
     override fun stringToData(string: String): E {
