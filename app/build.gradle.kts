3d2ac253e32b1eea2cdc8c7ebe8899700c12b734
Alen <1572777804@qq.com>
2024-05-10 12:18:20 +0800

    升级okhttp版本，升级kotlin版本，升级gradle版本，删除compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,12 +38,10 @@ android {
         jvmTarget = "1.8"
     }
     buildFeatures {
-        compose = true
+        compose = false
         viewBinding = true
     }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.10"
-    }
+
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
@@ -55,14 +53,10 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.8.0")
-    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3:1.1.1")
-    implementation ("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.12")
-    implementation ("com.squareup.okhttp3:okhttp-brotli:5.0.0-alpha.12")
+
+
+    implementation ("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.14")
+    implementation ("com.squareup.okhttp3:okhttp-brotli:5.0.0-alpha.14")
     implementation ("com.github.bumptech.glide:glide:4.16.0")
     //implementation ("com.github.bumptech.glide:annotations:4.16.0")
     implementation ("com.github.bumptech.glide:okhttp3-integration:4.16.0")
@@ -74,8 +68,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.5.0")
+
 }
--- oklibrary/build.gradle.kts
+++ oklibrary/build.gradle.kts
@@ -37,11 +37,9 @@ android {
         jvmTarget = "1.8"
     }
     buildFeatures {
-        compose = true
+        compose = false
     }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.10"
-    }
+
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
@@ -53,20 +51,12 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.7.0")
-    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3:1.1.1")
-    api ("com.squareup.okhttp3:okhttp:5.0.0-alpha.12")
+
+    api ("com.squareup.okhttp3:okhttp:5.0.0-alpha.14")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.5.0")
+    
 }
 
 
