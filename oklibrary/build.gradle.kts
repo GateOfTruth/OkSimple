33dba44ed5224c843a371e967b6b14e85a095fce
Alen <1572777804@qq.com>
2025-08-19 10:46:52 +0800

    升级版本到3.4.6

--- oklibrary/build.gradle.kts
+++ oklibrary/build.gradle.kts
@@ -73,7 +73,7 @@ publishing { // 发布配置
         register<MavenPublication>("release") { // 注册一个名字为 release 的发布内容
             groupId = "com.gateoftruth.library"
             artifactId = "OkSimple"
-            version = "3.4.3"
+            version = "3.4.6"
 
             afterEvaluate { // 在所有的配置都完成之后执行
                 // 从当前 module 的 release 包中发布
